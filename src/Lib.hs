9d3f51b7c8ba18602cbd63ee40ac2d372965404d
Magnus Møller <magnus108@me.com>
2022-04-05 16:58:53 +0200

    saveshit

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,10 +6,13 @@ where
 
 import qualified Graphics.UI.Threepenny        as UI
 import           Graphics.UI.Threepenny.Core
+                                         hiding ( delete )
 
 import           Item
 
 import qualified Relude.Unsafe                 as Unsafe
+import qualified Relude.Container.Reexport     as Reexport
+import qualified Relude.Extra.Map              as Map
 
 someFunc :: Int -> IO ()
 someFunc port = do
@@ -26,21 +29,20 @@ setup window = void $ mdo
     return window # set title "PhotoApp"
 
     -- GUI elements
-    createBtn   <- UI.button #+ [string "Create"]
-    deleteBtn   <- UI.button #+ [string "Delete"]
-    listBox <- UI.listBox bItems bSelection bDisplayItem
-    filterEntry <- UI.entry bFilterString
+    createBtn                         <- UI.button #+ [string "Create"]
+    deleteBtn                         <- UI.button #+ [string "Delete"]
+    listBox <- UI.listBox bListBoxItems bSelection bDisplayDataItem
+    filterEntry                       <- UI.entry bFilterString
 
-    entryName   <- UI.entry $ name <$> bItem
-    entryCode   <- UI.entry $ code <$> bItem
+    ((elemName, elemCode), tDataItem) <- dataItem bSelectionDataItem
 
-    let eCreate = UI.click createBtn
 
-    let items = grid
+    -- GUI layout
+    let uiDataItems = grid
             [ [ string "Name:"
-              , element entryName #. "input"
+              , element elemName #. "input"
               , string "Code:"
-              , element entryCode #. "input"
+              , element elemCode #. "input"
               ]
             ]
 
@@ -55,27 +57,120 @@ setup window = void $ mdo
                                  (attr "multiple")
                                  ""
                            ]
-                        , items
+                        , uiDataItems
                         ]
-                      , [row [element createBtn #. "button"]]
+                      , [ row
+                              [ element createBtn #. "button"
+                              , element deleteBtn #. "button"
+                              ]
+                        ]
                       ]
                 ]
            ]
 
+
+    -- Events and behaviors
     bFilterString <- stepper "" . rumors $ UI.userText filterEntry
 
-    bItem         <- stepper (Item "bob" "142A23") $ Unsafe.head <$> unions []
+    let
+        isInfixOf               :: (Eq a) => [a] -> [a] -> Bool
+        isInfixOf needle haystack = any (isPrefixOf needle) (tails haystack)
+    let tFilter = isInfixOf <$> UI.userText filterEntry
+        bFilter = facts tFilter
+        eFilter = rumors tFilter
 
-    bSelection    <- stepper Nothing $ Unsafe.head <$> unions []
+    let eSelection  = rumors $ UI.userSelection listBox
+        eDataItemIn = rumors $ tDataItem
+        eCreate     = UI.click createBtn
+        eDelete     = UI.click deleteBtn
 
-    bItems        <- accumB [] $ concatenate <$> unions
-        [create (Item "Emil" "1423123") <$ eCreate]
 
-    let bDisplayItem = pure (UI.string . name)
+    -- database
+    -- bDatabase :: Behavior (Database DataItem)
+    let update' mkey x = flip update x <$> mkey
+    bDatabase <- accumB emptydb $ concatenate <$> unions
+        [ create (Item "CAM" "0123dd12") <$ eCreate
+        , filterJust $ update' <$> bSelection <@> eDataItemIn
+        , delete <$> filterJust (bSelection <@ eDelete)
+        ]
 
-    onChanges bItems $ \items -> do
+    -- selection
+    -- bSelection :: Behavior (Maybe DatabaseKey)
+    bSelection <- stepper Nothing $ Unsafe.head <$> unions
+        [ eSelection
+        , Nothing <$ eDelete
+        , Just . nextKey <$> bDatabase <@ eCreate
+        , (\b s p -> b >>= \a -> if p (s a) then Just a else Nothing)
+        <$> bSelection
+        <*> bShowDataItem
+        <@> eFilter
+        ]
+
+    let bLookup :: Behavior (DatabaseKey -> Maybe DataItem)
+        bLookup = flip lookup <$> bDatabase
+
+        bShowDataItem :: Behavior (DatabaseKey -> String)
+        bShowDataItem    = (maybe "" showDataItem .) <$> bLookup
+
+        bDisplayDataItem = (UI.string .) <$> bShowDataItem
+
+        bListBoxItems :: Behavior [DatabaseKey]
+        bListBoxItems =
+            (\p show -> filter (p . show) . keys)
+                <$> bFilter
+                <*> bShowDataItem
+                <*> bDatabase
+
+        bSelectionDataItem :: Behavior (Maybe DataItem)
+        bSelectionDataItem = (=<<) <$> bLookup <*> bSelection
+
+
+    let bDisplayItem :: Behavior Bool
+        bDisplayItem = isJust <$> bSelection
+
+    element deleteBtn # sink UI.enabled bDisplayItem
+    element elemName # sink UI.enabled bDisplayItem
+    element elemCode # sink UI.enabled bDisplayItem
+
+    onChanges bDatabase $ \items -> do
         liftIO $ putStrLn (show items)
 
 
-create :: a -> [a] -> [a]
-create = (:)
+{-----------------------------------------------------------------------------
+    Database Model
+------------------------------------------------------------------------------}
+type DatabaseKey = Int
+data Database a  = Database { nextKey :: !Int, db :: Reexport.Map DatabaseKey a }
+        deriving (Show)
+
+
+emptydb = Database 0 mempty
+keys = Map.keys . db
+
+create x (Database newkey db) = Database (newkey + 1) $ Map.insert newkey x db
+update key x (Database newkey db) = Database newkey $ Map.insert key x db
+delete key (Database newkey db) = Database newkey $ Map.delete key db
+lookup key (Database _ db) = Map.lookup key db
+
+
+{-----------------------------------------------------------------------------
+    Data items that are stored in the data base
+------------------------------------------------------------------------------}
+
+type DataItem = Item
+showDataItem :: DataItem -> String
+showDataItem item = name item ++ ", " ++ (code item)
+
+emptyDataItem :: DataItem
+emptyDataItem = Item "" ""
+
+dataItem
+    :: Behavior (Maybe DataItem) -> UI ((Element, Element), Tidings DataItem)
+dataItem bItem = do
+    entry1 <- UI.entry $ name . fromMaybe emptyDataItem <$> bItem
+    entry2 <- UI.entry $ code . fromMaybe emptyDataItem <$> bItem
+
+    return
+        ( (getElement entry1, getElement entry2)
+        , Item <$> UI.userText entry1 <*> UI.userText entry2
+        )
